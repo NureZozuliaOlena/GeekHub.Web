4766c367d231e6db745233846a20a889b76a4259
llllenochkaaa <lenazozula05@gmail.com>
2024-07-21 18:44:42 +0300

    Create Add Blog Post page and submission form

---
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,22 @@
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages.Admin.Blogs
+{
+
+    public class AddModel : PageModel
+    {
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void onPost() 
+        {
+            
+        }
+    }
+}
