90790717f90698f42567fc5cfa66c9ef8987139e
llllenochkaaa <lenazozula05@gmail.com>
2024-07-28 16:48:13 +0300

    Create Identity DbContext

---
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var superAdminRoleId = "50b58de9-1d29-46e8-b861-89aa1b7a5dd8";
+            var adminRoleId = "9a050451-3785-4e48-b334-e79c0b6e3cbd";
+            var userRoleId = "14ffb160-fde1-4d42-b77f-7507a6271513";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Name = "SuperAdmin",
+                    NormalizedName = "SuperAdmin",
+                    Id = superAdminRoleId,
+                    ConcurrencyStamp = superAdminRoleId
+                },
+
+                new IdentityRole()
+                {
+                    Name = "Admin",
+                    NormalizedName = "Admin",
+                    Id = adminRoleId,
+                    ConcurrencyStamp = adminRoleId
+                },
+
+                new IdentityRole()
+                {
+                    Name = "User",
+                    NormalizedName = "User",
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            var superAdminId = "ace160ef-b8a8-4920-a818-8d86d42ce997";
+
+            var superAdminUser = new IdentityUser()
+            {
+                Id = superAdminId,
+                UserName = "superadmin@geekhub.com",
+                Email = "superadmin@geekhub.com"
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+                .HashPassword(superAdminUser, "superadmin123");
+
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            var superAdminRoles = new List<IdentityUserRole<string>>()
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = superAdminRoleId,
+                    UserId = superAdminId
+                },
+
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId
+                },
+
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = userRoleId
+                },
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+    }
+}
