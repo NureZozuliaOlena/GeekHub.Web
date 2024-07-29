9db52dffc0bdaefa1b8f9b140cd20e9a851ebb1c
llllenochkaaa <lenazozula05@gmail.com>
2024-07-29 12:18:19 +0300

    Implement Login page

---
+++ Bloggie.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,46 @@
+using GeekHub.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeekHub.Web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Login LoginViewModel { get; set; }
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(
+                LoginViewModel.Username, LoginViewModel.Password, false, false);
+
+            if (signInResult.Succeeded)
+            {
+                return RedirectToPage("Index");
+            }
+            
+            else
+            {
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Error,
+                    Message = "Unable to login"
+                };
+
+                return Page();
+            }
+        }
+    }
+}
