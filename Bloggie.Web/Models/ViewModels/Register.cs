14ae2692e60b9037bc46176a4119b49f386cd371
llllenochkaaa <lenazozula05@gmail.com>
2024-08-01 17:08:01 +0300

    Add validations for Register page

--- Bloggie.Web/Models/ViewModels/Register.cs
+++ Bloggie.Web/Models/ViewModels/Register.cs
@@ -1,9 +1,18 @@
-﻿namespace GeekHub.Web.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekHub.Web.Models.ViewModels
 {
     public class Register
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
--- Bloggie.Web/Pages/Register.cshtml.cs
+++ Bloggie.Web/Pages/Register.cshtml.cs
@@ -23,37 +23,45 @@ namespace GeekHub.Web.Pages
 
         public async Task<IActionResult> OnPost()
         {
-            var user = new IdentityUser
+            if (ModelState.IsValid)
             {
-                UserName = RegisterViewModel.Username,
-                Email = RegisterViewModel.Email,
-            };
-
-            var identityResult = await userManager.CreateAsync(user, RegisterViewModel.Password);
-
-            if (identityResult.Succeeded)
-            {
-                var addRolesResult = await userManager.AddToRoleAsync(user, "User");
-
-                if (addRolesResult.Succeeded)
+                var user = new IdentityUser
                 {
-                    ViewData["Notification"] = new Notification
-                    {
-                        Type = Enums.NotificationType.Success,
-                        Message = "User registered successfully"
-                    };
+                    UserName = RegisterViewModel.Username,
+                    Email = RegisterViewModel.Email,
+                };
 
-                    return Page();
+                var identityResult = await userManager.CreateAsync(user, RegisterViewModel.Password);
+
+                if (identityResult.Succeeded)
+                {
+                    var addRolesResult = await userManager.AddToRoleAsync(user, "User");
+
+                    if (addRolesResult.Succeeded)
+                    {
+                        ViewData["Notification"] = new Notification
+                        {
+                            Type = Enums.NotificationType.Success,
+                            Message = "User registered successfully"
+                        };
+
+                        return Page();
+                    }
                 }
+
+                ViewData["Notification"] = new Notification
+                {
+                    Type = Enums.NotificationType.Error,
+                    Message = "Something went wrong"
+                };
+
+                return Page();
             }
 
-            ViewData["Notification"] = new Notification
+            else
             {
-                Type = Enums.NotificationType.Error,
-                Message = "Something went wrong"
-            };
-
-            return Page();
+                return Page();
+            }
         }
     }
 }
